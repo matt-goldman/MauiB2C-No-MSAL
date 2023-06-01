1de1687a0867ddb71fc75e381ea10ca6625ef66d
Matt Goldman <matt.goldman@outlook.com>
2023-06-02 06:06:45 +1000

    Added t4 template

--- src/MAUI/MauiB2C/Constants.g.cs
+++ src/MAUI/MauiB2C/Constants.g.cs
@@ -1,5 +1,5 @@
 namespace MauiB2C;
 public static partial class Constants
 {
-	private static partial string GetTunnelUrl() => "https://dqmct2wb-7075.aue.devtunnels.ms/";
+	private static partial string GetTunnelUrl() => "Could not get tunnel URL";
 }
